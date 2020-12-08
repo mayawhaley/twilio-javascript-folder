e9fce098c0e887efb1db0cbf96bedf5445d4305c
mayawhaley <34068734+mayawhaley@users.noreply.github.com>
2020-12-08 09:24:56 -0500

    phase 2

--- romancalc.js
+++ romancalc.js
@@ -1,4 +1,45 @@
-// phase1
+// phase2
+
+// calculator 
+function calculate() {
+
+    // assigning variables from the get_args() in the calculate() function
+    var [first_num, operand, second_num] = get_args()
+
+    // convert strings to integers for calculation
+    var first = parseInt(first_num)
+    var second = parseInt(second_num)
+
+    var ans; // return variable that will contain answer
+
+    // if statements that check the operator type and calculates the answer
+    if (operand === "+") {
+        ans = first + second
+    }
+
+    if (operand === "-") {
+        ans = first - second
+    }
+
+    if (operand === "x") {
+        ans = first * second
+    }
+
+    if (operand === "/") {
+        ans = first / second
+    }
+
+    if (operand === "^") {
+        ans = first ** second
+    }
+
+    if (operand === "%") {
+        ans = first % second
+    }
+
+    return ans
+
+}
 
 // assigns arguments to their respective variables
 function get_args() {
@@ -7,10 +48,7 @@ function get_args() {
     var op = process.argv[3] // gets operator 
     var second = process.argv[4] // gets second number
 
-
     return [first, op, second] // return arguments
 }
-let op = get_args()
-console.log("The first operand is: ", op[0]);
-console.log("The second operand is: ", op[2]);
-console.log("The operator is: ", op[1]);
+
+console.log(calculate())
